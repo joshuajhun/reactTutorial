2ad8d2da17a1da3bc025208a22d2ddab3cc83902
joshuajhun <joshuajhun@gmail.com>
2016-05-11 11:59:15 -0700

    added mutiple components and working on adding props.

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,57 @@
 const React = require('react');
 const ReactDOM  = require('react-dom');
-// line StorePicker is a class
-// it will let us make a store picker <StorePicker/>
-var StorePicker = React.createClass({
+
+const App = React.createClass({
+  render :function(){
+  return(
+     <div className="catch-of-the-day">
+       <div class ="menu">
+         <Header />
+       </div>
+       <Order />
+       <Inventory />
+     </div>
+    )
+  }
+});
+
+const Header = React.createClass({
+  render :function(){
+    return(
+      <p>Header</p>
+    )
+  }
+})
+
+const Order = React.createClass({
+  render :function(){
+    return(
+      <p>Order</p>
+    )
+  }
+})
+
+const Inventory = React.createClass({
+  render :function(){
+    return(
+      <p>Inventory</p>
+    )
+  }
+})
+
+
+
+const StorePicker = React.createClass({
   render : function(){
     return (
-      <h1>hello</h1>
+      <form className="store-selector">
+        <h2> Please enter a store </h2>
+        <input type="text" ref="storeId" />
+        <input type="Submit" />
+      </form>
     )
   }
 });
 
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));
